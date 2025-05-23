513538da231aa24ab0ae5e8171ca51e91a4e060f
Hoang Pham <hoanglk12@gmail.com>
2025-05-23 23:54:01 +0700

    update test

--- tests/facebook.spec.js
+++ tests/facebook.spec.js
@@ -27,7 +27,7 @@ test.describe('Facebook Homepage Tests', () => {
   });
 
   test('should show error for empty email login attempt', async ({ page }) => {
-    await page.goto('https://www.facebook.com/');
+    await page.refresh();
     await page.locator('#pass').fill('123456');
     await page.locator('//button[@name="login"]').click();
     await page.waitForLoadState('networkidle');
